ca911acc9c95fa77c957466738989c1cdf87771b
maueval <18522973+maueval@users.noreply.github.com>
2018-09-15 00:30:18 +0900

    Modify 0015

--- vol00/0015_National Budget.py
+++ vol00/0015_National Budget.py
@@ -1,23 +1,5 @@
-from collections import deque
-
-"""
-# Too slow
-def add2(a, b):
-    a, b = align_digit(a, b)
-    zipped = zip(a[::-1], b[::-1])
-    carry = 0
-    result = []
-    for a, b in zipped:
-        s = int(a) + int(b) + carry
-        carry = int(s / 10)
-        result.append(str(s % 10))
-    if result[-1] == '0':
-        result[-1] = str(carry)
-    print(''.join(result[::-1]))
-"""
-
-
-def align_digit(a, b):
+# 12,432 -> 012,432
+def align_digits(a, b):
     la, lb = len(a), len(b)
     if la < lb:
         a = a.zfill(lb)
@@ -26,38 +8,29 @@ def align_digit(a, b):
     return a, b
 
 
-def split_to_ints(s, n=10):
-    length = len(s)
-    return [int(s[i-n:i]) for i in range(length, 0, -n)]
-
-
-def add(a, b, n=10):
-    a, b = align_digit(a, b)
-    print(a, b)
-    ints_a = split_to_ints(a, n)
-    ints_b = split_to_ints(b, n)
+def add(a, b):
+    a, b = align_digits(a, b)
+    zipped = list(zip(a[::-1], b[::-1]))
     carry = 0
     result = []
-
-    zipped = list(zip(ints_a, ints_b))[::-1]
-    print(zipped)
-    for i, (int_a, int_b) in enumerate(zipped):
-        s = int_a + int_b + carry
-        carry = int(s / (10 ** n))
-        if i != len(zipped) - 1:
-            result.append(str(s % (10 ** (n - 1))).zfill(n))
-        else:
+    for i, z in enumerate(zipped):
+        a, b = z
+        s = int(a) + int(b) + carry
+        carry = int(s / 10)
+        if i == len(zipped) - 1:
             result.append(str(s))
-
-    print(result)
-    ret = str(int(''.join(reversed(result))))
-    if len(ret) > 80:
+        else:
+            result.append(str(s % 10))
+    ans = ''.join(result[::-1])
+    if len(ans) > 80:
         return 'overflow'
-    return ret
+    return ans
 
 
 if __name__ == '__main__':
     n_input = int(input())
+    inputs = []
     for i in range(n_input):
-        a, b = input(), input()
+        inputs.append((input(), input()))
+    for a, b in inputs:
         print(add(a, b))
